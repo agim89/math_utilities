a33bfca2a71d7e8aab3cd0b256277148a0f07ae2
Agim Kamberi <agim.kamberi@avenga.com>
2026-01-16 09:34:33 +0100

    Yaml files adjusted

--- include/mylib/sum.h
+++ include/mylib/sum.h
@@ -1,11 +1,2 @@
-#ifndef MYLIB_H
-#define MYLIB_H
-
-namespace mylib {
-
-    int sum(int a, int b);
-int multiply(int a, int b);
-
-} // namespace mylib
-
-#endif // MYLIB_H
+#pragma once
+int sum(int a, int b);
--- src/sum.cpp
+++ src/sum.cpp
@@ -1,11 +1,5 @@
-#include "mylib/mylib.h"
-
-namespace mylib {
+#include "mylib/sum.h"
 
 int sum(int a, int b) {
     return a + b;
-}
-
-int multiply(int a, int b) {
-    return a * b;
 }
